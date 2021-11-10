dffdea9cb78a612f482ffbc25467bfbd8f1b9145
Sway Wang <sway.khwang@gmail.com>
2021-11-09 23:38:44 -0800

    Add two C# implementations of Observer and Singleton pattern

---
+++ c_sharp/C_sharp_ovserver/Observer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SampleAP
+{
+    interface Observer
+    {
+        void Update(Subject obj, object arg);
+    }
+}
---
+++ c_sharp/C_sharp_ovserver/Subject.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SampleAP
+{
+    abstract class Subject
+    {
+        public delegate void StatusUpdate(Subject obj, object arg);
+        public event StatusUpdate OnStatusUpdate = null;
+        public bool changed = false;
+
+        public void addObserver(Observer o)
+        {
+            OnStatusUpdate += new StatusUpdate(o.Update);
+        }
+        
+        public void deleteObserver(Observer o)
+        {
+            OnStatusUpdate -= new StatusUpdate(o.Update);
+        }
+
+        public void notifyObservers(Object arg)
+        {
+            if (OnStatusUpdate != null && changed)
+            {
+                OnStatusUpdate(this, arg);
+            }
+
+            changed = false;
+        }
+
+        public void notifyObservers()
+        {
+            notifyObservers(null);
+        }
+
+        public void setChanged()
+        {
+            changed = true;
+        }
+    }
+}
